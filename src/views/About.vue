a9931c53cfb1abe1cc2a1a81e50e43f55faef2f0
hannah68 <reihaneh.naderi1989@gmail.com>
2022-05-06 13:21:07 +0100

    init app

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas quam assumenda harum itaque ex in aut sint dicta vitae molestiae? Eveniet magnam eum minima illum eos suscipit saepe! Debitis odit corporis ratione, pariatur voluptas magni a mollitia, sint vero ipsa natus! Distinctio, facilis eligendi? Eligendi numquam totam ex unde iure?</p>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Home Page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eligendi dolores sed vitae voluptates enim deleniti ad fugit placeat perspiciatis.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, dolor totam quo, adipisci dignissimos debitis, rem laudantium voluptas dolorem iusto provident. Beatae, id laudantium ea at sint reprehenderit expedita hic officiis? Beatae ratione, minus amet suscipit sequi ab ullam consequatur.</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
